ba05a2f6f653a500aa4fae792e5d5f65d25e05d0
comecsoftdev <comecsoftdev@gmail.com>
2022-03-04 09:57:43 +0900

    1. custom_exception_handler에서 리턴되는 status 값을 변경   - system exception인 경우 system exception의 status값으로 리턴

--- flashsale/misc/lib/extends.py
+++ flashsale/misc/lib/extends.py
@@ -3,10 +3,10 @@ from rest_framework.response import Response
 
 
 def custom_exception_handler(exc, context):
-    _status = status.HTTP_200_OK
+    _status = status.HTTP_404_NOT_FOUND
 
     message = exc.detail.__str__() if hasattr(exc, 'detail') else exc.__str__()
-    _status = exc.result_code if hasattr(exc, 'result_code') else 520
+    _status = exc.result_code if hasattr(exc, 'result_code') else exc.status_code
 
     data = {'msg': message}
 
