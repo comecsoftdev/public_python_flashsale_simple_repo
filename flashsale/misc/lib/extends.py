f3687b62a8bd4870042e9207f8797e446de46b08
comecsoftdev <comecsoftdev@gmail.com>
2022-03-08 09:10:23 +0900

    1. custom_exception_handler에서 리턴되는 status 값을 변경   - exception중에서 status_code를 포함하지 않는 exception은 default status 리턴

--- flashsale/misc/lib/extends.py
+++ flashsale/misc/lib/extends.py
@@ -6,7 +6,7 @@ def custom_exception_handler(exc, context):
     _status = status.HTTP_404_NOT_FOUND
 
     message = exc.detail.__str__() if hasattr(exc, 'detail') else exc.__str__()
-    _status = exc.result_code if hasattr(exc, 'result_code') else exc.status_code
+    _status = exc.result_code if hasattr(exc, 'result_code') else getattr(exc, 'status_code', _status)
 
     data = {'msg': message}
 
